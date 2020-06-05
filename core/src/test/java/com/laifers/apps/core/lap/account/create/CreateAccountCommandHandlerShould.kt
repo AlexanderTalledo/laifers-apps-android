b0b38567be89c2e4266aafb39c98e32019f18c37
Alex Talledo <alexta.dev@gmail.com>
2020-06-05 12:39:48 +0200

    Replace repetitive input unit tests by parameterized unit tests

--- core/src/test/java/com/laifers/apps/core/lap/account/create/CreateAccountCommandHandlerShould.kt
+++ core/src/test/java/com/laifers/apps/core/lap/account/create/CreateAccountCommandHandlerShould.kt
@@ -9,6 +9,8 @@ import com.laifers.apps.core.shared.domain.InvalidValue
 import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.reflect.KClass
 
 class CreateAccountCommandHandlerShould : AccountModuleUnitTestCase() {
@@ -22,230 +24,73 @@ class CreateAccountCommandHandlerShould : AccountModuleUnitTestCase() {
         @Nested
         internal inner class Input {
 
-            @Test
-            fun `when account id input value is empty`() {
-                val id = ""
-
+            @ParameterizedTest
+            @ValueSource(
+                strings = [
+                    "",
+                    " ",
+                    "ufa 98a 9 8y asdjf",
+                    "25a29306-05e3-44a2-b79b-"
+                ]
+            )
+            fun `when account id value is invalid`(id: String) {
                 testInvalidCreateAccountCommand(
                     provideCustomizableCreateAccountCommand(id = id),
                     provideInvalidAccountIdErrorMessage(id)
                 )
             }
 
-            @Test
-            fun `when account id input value is whitespaces only`() {
-                val id = "    "
 
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(id = id),
-                    provideInvalidAccountIdErrorMessage(id)
-                )
-            }
-
-            @Test
-            fun `when account id input value is a random text`() {
-                val id = "adf98d dafhiadsf9y adsf9yasydsaf joh9u"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(id = id),
-                    provideInvalidAccountIdErrorMessage(id)
-                )
-            }
-
-            @Test
-            fun `when account id input value is an incomplete id`() {
-                val id = "25a29306-05e3-44a2-b79b-"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(id = id),
-                    provideInvalidAccountIdErrorMessage(id)
-                )
-            }
-
-            @Test
-            fun `when account username input value is empty`() {
-                val username = ""
-
+            @ParameterizedTest
+            @ValueSource(
+                strings = [
+                    "",
+                    "   ",
+                    "ldf 089fd 90240u9 ga kkkppa",
+                    "a",
+                    "1",
+                    "ab=#$&"
+                ]
+            )
+            fun `when account username value is invalid`(username: String) {
                 testInvalidCreateAccountCommand(
                     provideCustomizableCreateAccountCommand(username = username),
                     provideInvalidUsernameErrorMessage(username)
                 )
             }
 
-            @Test
-            fun `when account username input value is whitespaces only`() {
-                val username = "     "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(username = username),
-                    provideInvalidUsernameErrorMessage(username)
-                )
-            }
-
-            @Test
-            fun `when account username input value is a random text`() {
-                val username = "ldf 089fd 90240u9 ga kkkppa"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(username = username),
-                    provideInvalidUsernameErrorMessage(username)
-                )
-            }
-
-            @Test
-            fun `when account username input value has no minimum required length`() {
-                val username = "ab"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(username = username),
-                    provideInvalidUsernameErrorMessage(username)
-                )
-            }
-
-            @Test
-            fun `when account username input value contains not allowed special characters`() {
-                val username = "ab=#$&"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(username = username),
-                    provideInvalidUsernameErrorMessage(username)
-                )
-            }
-
-            @Test
-            fun `when account email address input value is empty`() {
-                val emailAddress = ""
-
+            @ParameterizedTest
+            @ValueSource(
+                strings = [
+                    "",
+                    "       ",
+                    "fdauhd 8d89sf dasfbhkadbhfadd797 ",
+                    "useremail.com",
+                    "user@.com",
+                    "user@email"
+                ]
+            )
+            fun `when account email address value is invalid`(emailAddress: String) {
                 testInvalidCreateAccountCommand(
                     provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
                     provideInvalidEmailAddressErrorMessage(emailAddress)
                 )
             }
 
-            @Test
-            fun `when account email address input value is whitespaces only`() {
-                val emailAddress = "       "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
-                    provideInvalidEmailAddressErrorMessage(emailAddress)
-                )
-            }
-
-            @Test
-            fun `when account email address input value is a random text`() {
-                val emailAddress = "fdauhd 8d89sf dasfbhkadbhfadd797 "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
-                    provideInvalidEmailAddressErrorMessage(emailAddress)
-                )
-            }
-
-            @Test
-            fun `when account email address input value has not an @ symbol`() {
-                val emailAddress = "useremail.com "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
-                    provideInvalidEmailAddressErrorMessage(emailAddress)
-                )
-            }
-
-            @Test
-            fun `when account email address input value has no server name`() {
-                val emailAddress = "user@.com "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
-                    provideInvalidEmailAddressErrorMessage(emailAddress)
-                )
-            }
-
-            @Test
-            fun `when account email address input value has no domain`() {
-                val emailAddress = "user@email"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(emailAddress = emailAddress),
-                    provideInvalidEmailAddressErrorMessage(emailAddress)
-                )
-            }
-
-            @Test
-            fun `when account password input value is empty`() {
-                val password = ""
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password input value is whitespaces only`() {
-                val password = "        "
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has no minimum required length`() {
-                val password = "Abc123@"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has no required at least one number`() {
-                val password = "ABCDefgh!"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has no required at least one uppercase character`() {
-                val password = "abcdefgh@435"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has no required at least one lowercase character`() {
-                val password = "SDFFDUH2343!@"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has no required at least one special character`() {
-                val password = "AOiih9889VDccs"
-
-                testInvalidCreateAccountCommand(
-                    provideCustomizableCreateAccountCommand(password = password),
-                    provideInvalidPasswordErrorMessage(password)
-                )
-            }
-
-            @Test
-            fun `when account password has not allowed special characters`() {
-                val password = "AOiih9889VDccs?¿"
-
+            @ParameterizedTest
+            @ValueSource(
+                strings = [
+                    "",
+                    "        ",
+                    "Abc123@",
+                    "ABCDefgh!",
+                    "abcdefgh@435",
+                    "SDFFDUH2343!@",
+                    "AOiih9889VDccs",
+                    "AOiih9889VDccs?¿"
+                ]
+            )
+            fun `when account password value is invalid`(password: String) {
                 testInvalidCreateAccountCommand(
                     provideCustomizableCreateAccountCommand(password = password),
                     provideInvalidPasswordErrorMessage(password)
